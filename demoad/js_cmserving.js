4c1f493287d584ac1af1ff680ff71c48513e3c53
Carol Chaw <carchaw@publicisgroupe.net>
2021-07-16 14:47:45 +0800

    fs test37

--- demoad/js_cmserving.js
+++ demoad/js_cmserving.js
@@ -52,19 +52,17 @@ function addPixel() {
   </div>`;
     window.parent.document.body.append(bannerDiv);
     var pixel = document.getElementById('pfx_pixel');
+    var banner = document.getElementById('pfx_banner');
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (!expanded && !entry.isIntersecting) {
+          banner.style.display = "block";
+          // googletag.cmd.push(function () { googletag.pubads().refresh(); });
+          expanded = true;
+        }
+      });
+    }, {});
+    observer.observe(pixel);
 
-    if (pixel) {
-      var banner = document.getElementById('pfx_banner');
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-          if (!expanded && !entry.isIntersecting) {
-            banner.style.display = "block";
-            googletag.cmd.push(function () { googletag.pubads().refresh(); });
-            expanded = true;
-          }
-        });
-      }, {});
-      observer.observe(pixel);
-    }
   }
 }
