3ed6a1a0a1eedbf530fd473198ecfb1ba03efd99
Carol Chaw <carchaw@publicisgroupe.net>
2021-07-16 12:24:26 +0800

    fs test25

--- demoad/js_cmserving.js
+++ demoad/js_cmserving.js
@@ -38,19 +38,7 @@ function addPixel() {
   window.parent.document.head.append(headJs);
   window.parent.document.head.append(headJsFunc);
 
-  var bannerDiv = document.createElement("div");
-  bannerDiv.setAttribute("id", "pfx_banner");
-  bannerDiv.setAttribute(
-    "style",
-    "position:fixed;left:0;z-index:999999;bottom:0;width:100%;height:100%;max-height:260px;display:none;"
-  );
-  bannerDiv.innerHTML += `<div id='div-gpt-ad-1626335725009-0' style='min-width: 414px; min-height: 260px;'>
-    <script>
-      googletag.cmd.push(function() { googletag.display('div-gpt-ad-1626335725009-0'); });
-    </script>
-  </div>`;
 
-  window.parent.document.body.append(bannerDiv);
   adDone = true;
   console.log(adDone)
 
