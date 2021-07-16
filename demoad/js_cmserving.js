8020d9880891bfc484d8d4b354461f6b4f711805
Carol Chaw <carchaw@publicisgroupe.net>
2021-07-16 13:02:11 +0800

    fs test289

--- demoad/js_cmserving.js
+++ demoad/js_cmserving.js
@@ -35,9 +35,22 @@ function addPixel() {
   headJsFunc.append(headJstext)
   window.parent.document.head.append(headJs);
   window.parent.document.head.append(headJsFunc);
-  // adDone = true;
+  adDone = true;
   if (adDone) {
-    expand();
+
+    var bannerDiv = document.createElement("div");
+    bannerDiv.setAttribute("id", "pfx_banner");
+    bannerDiv.setAttribute(
+      "style",
+      "position:fixed;left:0;z-index:999999;bottom:0;width:100%;height:100%;max-height:260px;display:none;"
+    );
+    bannerDiv.innerHTML += `<div id='div-gpt-ad-1626335725009-0' style='min-width: 414px; min-height: 260px;'>
+    <script>
+      googletag.cmd.push(function() { googletag.display('div-gpt-ad-1626335725009-0'); });
+    </script>
+  </div>`;
+    window.parent.document.body.append(bannerDiv);
+
   }
 
 }
@@ -45,20 +58,6 @@ function addPixel() {
 function expand() {
   var expanded = false;
   var pixel = document.getElementById('pfx_pixel');
-  var bannerDiv = document.createElement("div");
-  bannerDiv.setAttribute("id", "pfx_banner");
-  bannerDiv.setAttribute(
-    "style",
-    "position:fixed;left:0;z-index:999999;bottom:0;width:100%;height:100%;max-height:260px;display:none;"
-  );
-  bannerDiv.innerHTML += `<div id='div-gpt-ad-1626335725009-0' style='min-width: 414px; min-height: 260px;'>
-    <script>
-      googletag.cmd.push(function() { googletag.display('div-gpt-ad-1626335725009-0'); });
-    </script>
-  </div>`;
-
-  window.parent.document.body.append(bannerDiv);
-
   var banner = document.getElementById('pfx_banner');
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -69,7 +68,6 @@ function expand() {
       }
     });
   }, {});
-
 
   observer.observe(pixel);
 }
