12c9d4f0402dc17bee21320ccb4686e6798b1f16
Carol Chaw <carchaw@publicisgroupe.net>
2021-07-16 15:23:55 +0800

    fs test40

--- demoad/js_cmserving.js
+++ demoad/js_cmserving.js
@@ -38,35 +38,43 @@ function addPixel() {
   adDone = true;
 
   if (adDone) {
-    var pixel = document.getElementById('pfx_pixel');
-    var bannerDiv = document.createElement("div");
-    bannerDiv.setAttribute("id", "pfx_banner");
-    bannerDiv.setAttribute(
-      "style",
-      "position:fixed;left:0;z-index:999999;bottom:0;width:100%;height:100%;max-height:260px;display:none;"
-    );
-    bannerDiv.innerHTML += `<div id='div-gpt-ad-1626335725009-0' style='min-width: 414px; min-height: 260px;'>
+
+
+    if (!pixel) {
+      setTimeout(() => {
+        bannerappend()
+      }, 500);
+    } else {
+      console.log("detect pixel")
+      bannerappend()
+    }
+
+  }
+}
+
+function bannerappend() {
+  var pixel = document.getElementById('pfx_pixel');
+  var bannerDiv = document.createElement("div");
+  bannerDiv.setAttribute("id", "pfx_banner");
+  bannerDiv.setAttribute(
+    "style",
+    "position:fixed;left:0;z-index:999999;bottom:0;width:100%;height:100%;max-height:260px;display:none;"
+  );
+  bannerDiv.innerHTML += `<div id='div-gpt-ad-1626335725009-0' style='min-width: 414px; min-height: 260px;'>
     <script>
       googletag.cmd.push(function() { googletag.display('div-gpt-ad-1626335725009-0'); });
     </script>
   </div>`;
-    window.parent.document.body.append(bannerDiv);
+  window.parent.document.body.append(bannerDiv);
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (!expanded && !entry.isIntersecting) {
-          banner.style.display = "block";
-          // googletag.cmd.push(function () { googletag.pubads().refresh(); });
-          expanded = true;
-        }
-      });
-    }, {});
-
-    if (pixel) {
-      observer.observe(pixel);
-    } else {
-      console.log("no detect pixel")
-    }
-
-  }
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (!expanded && !entry.isIntersecting) {
+        banner.style.display = "block";
+        // googletag.cmd.push(function () { googletag.pubads().refresh(); });
+        expanded = true;
+      }
+    });
+  }, {});
+  observer.observe(pixel);
 }
