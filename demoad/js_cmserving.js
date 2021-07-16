1794a2c96b3799c806d5aee408bfb325d37e3b45
Carol Chaw <carchaw@publicisgroupe.net>
2021-07-16 18:01:06 +0800

    fs test54

--- demoad/js_cmserving.js
+++ demoad/js_cmserving.js
@@ -1,30 +1,27 @@
-var adDone = false;
-var expanded = false;
-if (!adDone) {
-  addPixel();
-}
+(function () {
+  var expanded = false;
 
-function addPixel() {
-  var pixelDiv = document.createElement("div");
-  pixelDiv.setAttribute("id", "pfx_pixel");
-  pixelDiv.setAttribute(
-    "style",
-    "background-color:red;width:1px;height:1px;position:absolute;top:0;left:0;"
-  );
+  function addPixel() {
+    var pixelDiv = document.createElement("div");
+    pixelDiv.setAttribute("id", "pfx_pixel");
+    pixelDiv.setAttribute(
+      "style",
+      "background-color:red;width:1px;height:1px;position:absolute;top:0;left:0;"
+    );
 
-  var script = document.createElement("script");
-  script.src = "https://badcarolwork.github.io/ccdemopage/demoad/cmserving_func.js";
-  window.parent.document.body.append(pixelDiv);
+    var script = document.createElement("script");
+    script.src = "https://badcarolwork.github.io/ccdemopage/demoad/cmserving_func.js";
+    window.parent.document.body.append(pixelDiv);
 
-  window.parent.document.body.append(script);
+    window.parent.document.body.append(script);
 
-  //append banner
+    //append banner
 
-  var headJs = document.createElement("script");
-  headJs.src = "https://securepubads.g.doubleclick.net/tag/js/gpt.js";
-  var headJsFunc = document.createElement("script");
+    var headJs = document.createElement("script");
+    headJs.src = "https://securepubads.g.doubleclick.net/tag/js/gpt.js";
+    var headJsFunc = document.createElement("script");
 
-  var headJstext = `
+    var headJstext = `
   window.googletag = window.googletag || {cmd: []};
   var adslot1;
   googletag.cmd.push(function() {
@@ -33,37 +30,23 @@ function addPixel() {
     googletag.enableServices();
   });`
 
-  headJsFunc.append(headJstext)
-  window.parent.document.head.append(headJs);
-  window.parent.document.head.append(headJsFunc);
+    headJsFunc.append(headJstext)
+    window.parent.document.head.append(headJs);
+    window.parent.document.head.append(headJsFunc);
 
-  var bannerDiv = document.createElement("div");
-  bannerDiv.setAttribute("id", "pfx_banner");
-  bannerDiv.setAttribute(
-    "style",
-    "position:fixed;left:0;z-index:999999;bottom:0;width:100%;height:100%;max-height:260px;display:none;"
-  );
-  bannerDiv.innerHTML += `<div id='div-gpt-ad-1626335725009-0' style='min-width: 414px; min-height: 260px;'>
+    var bannerDiv = document.createElement("div");
+    bannerDiv.setAttribute("id", "pfx_banner");
+    bannerDiv.setAttribute(
+      "style",
+      "position:fixed;left:0;z-index:999999;bottom:0;width:100%;height:100%;max-height:260px;display:none;"
+    );
+    bannerDiv.innerHTML += `<div id='div-gpt-ad-1626335725009-0' style='min-width: 414px; min-height: 260px;'>
     <script>
       googletag.cmd.push(function() { googletag.display('div-gpt-ad-1626335725009-0'); });
     </script>
   </div>`;
-  window.parent.document.body.append(bannerDiv);
+    window.parent.document.body.append(bannerDiv);
+  }
 
-
-  // const observer = new IntersectionObserver((entries) => {
-  //   entries.forEach((entry) => {
-  //     if (!expanded && !entry.isIntersecting) {
-  //       banner.style.display = "block";
-  //       googletag.cmd.push(function () {
-  //         googletag.pubads().refresh([adslot1]);
-  //       });
-  //       expanded = true;
-  //     }
-  //   });
-  // }, {});
-  // observer.observe(pixel);
-
-
-
-}
+  addPixel();
+})();
