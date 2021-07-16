6b1939f5879ac800f10bc1808011ac35b578648b
Carol Chaw <carchaw@publicisgroupe.net>
2021-07-16 17:39:48 +0800

    fs test52

--- demoad/js_cmserving.js
+++ demoad/js_cmserving.js
@@ -50,20 +50,19 @@ function addPixel() {
   </div>`;
   window.parent.document.body.append(bannerDiv);
 
-  var pixel = document.getElementById('pfx_pixel');
-  var banner = document.getElementById('pfx_banner');
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (!expanded && !entry.isIntersecting) {
-        banner.style.display = "block";
-        googletag.cmd.push(function () {
-          googletag.pubads().refresh([adslot1]);
-        });
-        expanded = true;
-      }
-    });
-  }, {});
-  observer.observe(pixel);
+
+  // const observer = new IntersectionObserver((entries) => {
+  //   entries.forEach((entry) => {
+  //     if (!expanded && !entry.isIntersecting) {
+  //       banner.style.display = "block";
+  //       googletag.cmd.push(function () {
+  //         googletag.pubads().refresh([adslot1]);
+  //       });
+  //       expanded = true;
+  //     }
+  //   });
+  // }, {});
+  // observer.observe(pixel);
 
 
 
