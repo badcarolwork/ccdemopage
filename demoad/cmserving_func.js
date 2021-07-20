cdb650a1e2f8c940b98c3e2aa0a4c753f998c238
Carol Chaw <carchaw@publicisgroupe.net>
2021-07-20 14:37:58 +0800

    fs test61 post1

--- demoad/cmserving_func.js
+++ demoad/cmserving_func.js
@@ -3,34 +3,23 @@ var pixel = document.getElementById('pfx_pixel');
 var banner = document.getElementById('pfx_banner');
 
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (!expanded && !entry.isIntersecting) {
-      banner.style.display = "block";
-      googletag.cmd.push(function () { googletag.pubads().refresh([adslot1]); });
-      expanded = true;
-      closeMsg();
-    }
-  });
+	entries.forEach((entry) => {
+		if (!expanded && !entry.isIntersecting) {
+			banner.style.display = "block";
+			googletag.cmd.push(function () { googletag.pubads().refresh([adslot1]); });
+			expanded = true;
+			closeMsg();
+		}
+	});
 }, {});
 
 
 observer.observe(pixel);
 
+window.addEventListener('message', e => {
+	console.log(e.data);
+	if (e.data == "userClose" || e.message === "userClose") {
+		alert('Message from iframe just came!');
+	}
+}, false);
 
-
-function closeMsg(){
-  var eventMethod = window.addEventListener
-			? "addEventListener"
-			: "attachEvent";
-	var eventer = window[eventMethod];
-	var messageEvent = eventMethod === "attachEvent"
-		? "onmessage"
-		: "message";
-
-	eventer(messageEvent, function (e) {		
-		// if (e.origin !== 'http://the-trusted-iframe-origin.com') return;		
-		if (e.data === "userClose" || e.message === "userClose") 
-			alert('Message from iframe just came!');		
-		  console.log(e);
-	});
-}
