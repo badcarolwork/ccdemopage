bccbf188e0ed8a549eacbb7e36296763d36e096b
Carol Chaw <carchaw@publicisgroupe.net>
2021-07-20 16:49:27 +0800

    fs test61 post9

--- demoad/js_cmserving.js
+++ demoad/js_cmserving.js
@@ -23,7 +23,7 @@
     var headJstext = `window.googletag = window.googletag || {cmd: []};
     var adslot1;
     googletag.cmd.push(function() {
-    adslot1 = googletag.defineSlot(''/21669303496/cctest_fs_414260', [1, 1], 'div-gpt-ad-1626768268989-0').setTargeting('test', 'refresh').addService(googletag.pubads());
+    adslot1 = googletag.defineSlot('/21669303496/cctest_fs_414260', [1, 1], 'div-gpt-ad-1626768268989-0').setTargeting('test', 'refresh').addService(googletag.pubads());
     googletag.pubads().enableSingleRequest();
     googletag.enableServices();
   });`
