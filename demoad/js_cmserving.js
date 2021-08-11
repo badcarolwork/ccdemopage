d0a15be0612e1c66c53dff218a25f940cee0c304
userA <carchaw@publicisgroupe.net>
2021-08-11 16:16:23 +0800

    round25

--- demoad/js_cmserving.js
+++ demoad/js_cmserving.js
@@ -19,9 +19,12 @@
     var headJsFunc = document.createElement("script");
 
     var headJstext = `window.googletag = window.googletag || {cmd: []};
-    googletag.cmd.push(function() {
-      googletag.defineSlot('/21669303496/cctest_fs_414260', [1, 1], 'div-gpt-ad-1628655025326-0').addService(googletag.pubads());
-    googletag.enableServices();});`
+      googletag.cmd.push(function() {
+        googletag.defineSlot('/21669303496/cctest_fs_414260', [1, 1], 'div-gpt-ad-1628655025326-0').addService(googletag.pubads());
+        googletag.pubads().enableSingleRequest();
+        googletag.enableServices();
+      });
+    </script>`
 
     headJsFunc.append(headJstext)
     window.parent.document.head.append(headJsFunc);
@@ -29,7 +32,11 @@
     var bannerDiv = document.createElement("div");
     bannerDiv.setAttribute("id", "pfx_banner");
     bannerDiv.setAttribute("style", "position:fixed;left:0;z-index:999999;bottom:0;width:100%;height:100%;max-height:260px;display:none;");
-    bannerDiv.innerHTML += `<div id='div-gpt-ad-1628498031248-0'><script>googletag.cmd.push(function() { googletag.display('div-gpt-ad-1628655025326-0'); });</script></div>`;
+    bannerDiv.innerHTML += `<div id='div-gpt-ad-1628655025326-0'>
+      <script>
+        googletag.cmd.push(function() { googletag.display('div-gpt-ad-1628655025326-0'); });
+      </script>
+    </div>`;
     window.parent.document.body.append(bannerDiv);
   }
 
