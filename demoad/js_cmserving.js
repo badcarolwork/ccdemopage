4ecc487124313c5ba7de62c08621f130e6fd1a5f
userA <carchaw@publicisgroupe.net>
2021-08-11 15:14:58 +0800

    round19

--- demoad/js_cmserving.js
+++ demoad/js_cmserving.js
@@ -32,7 +32,7 @@
     var bannerDiv = document.createElement("div");
     bannerDiv.setAttribute("id", "pfx_banner");
     bannerDiv.setAttribute("style", "position:fixed;left:0;z-index:999999;bottom:0;width:100%;height:100%;max-height:260px;display:none;");
-    bannerDiv.innerHTML += `<div id='div-gpt-ad-1628498031248-0' style='min-width: 414px; min-height: 260px;'>
+    bannerDiv.innerHTML += `<div id='div-gpt-ad-1628498031248-0'>
     <script>
     googletag.cmd.push(function() { googletag.display('div-gpt-ad-1628655025326-0'); });
     </script>
